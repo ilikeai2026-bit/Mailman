2fa92fc9d8465e5f252eca7935c584bdd4c19933
I Like AI <i.like.ai.2026@gmail.com>
2026-09-06 19:17:38 +0300

    Set relative base for Mailman repo

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/Voice-Browser-Game/',
-}));
+export default defineConfig({
+  base: './',
+});
